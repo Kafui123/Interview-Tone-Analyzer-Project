7612c7d9c5f29cc3797854c2552fc08aac414272
glek <156453486+Kafui123@users.noreply.github.com>
2025-05-28 22:52:53 -0400

    Changes

--- transcriber.py
+++ transcriber.py
@@ -1,7 +1,10 @@
-import whisper 
+import whisper
 
-
+# use the .load_model function in whisper to convert your file to english 
 model = whisper.load_model("base")
 result = model.transcribe("WhatsApp Ptt 2025-05-24 at 21.08.19.ogg")
 
-print(result["text"])
+# save to text file 
+
+with open("transcription.txt", "w", encoding="utf-8") as f:
+    f.write(result["text"])
